cb85143790c0302bfce1183a6421de3c7b5a7bfc
An Ngo <ngoan_230@yahoo.com>
2017-04-27 02:48:45 -0400

    set onclick function to append text

--- assets/javascripts/javascript.js
+++ assets/javascripts/javascript.js
@@ -9,3 +9,11 @@ document.getElementById("text").innerHTML = "Hello An!";
 document.getElementById("myButton").onclick = function() {
 	document.getElementById("text").innerHTML = "Hello Rob!";
 }
+
+document.getElementById("myButton-2").onclick = function() { 
+	document.getElementById("secondParagraph").innerHTML = "I think " + document.getElementById("secondParagraph").innerHTML + "awesome!";
+}
+
+document.getElementById("myButton-3").onclick = function() {
+	document.getElementById("emptyParagraph").innerHTML = "Magic!";
+}
