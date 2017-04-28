3d90760b100583102a53af722ba5cb8c2a746725
An Ngo <ngoan_230@yahoo.com>
2017-04-28 04:29:03 -0400

    using variables

--- assets/javascripts/miniChallengeJS.js
+++ assets/javascripts/miniChallengeJS.js
@@ -9,3 +9,20 @@ document.getElementById("blue").onclick = function() {
 document.getElementById("yellow").onclick = function() {
 	document.getElementById("yellow").style.display = "none";
 }
+
+document.getElementById("poof").onclick = function() {
+	var textEntered = "";
+
+	textEntered = document.getElementById("textInput").value;
+
+	document.getElementById("changeThis").innerHTML = textEntered;
+
+}
+
+var x = 5;
+
+x = 20;
+
+var name = "An";
+
+alert(name);
